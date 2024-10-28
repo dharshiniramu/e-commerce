0d4bf96b8db3945ca23b390fee1bbb79400d47a0
dharshiniramu <dharshiniramu78@gmail.com>
2024-10-28 10:32:12 +0530

    haeder changed

--- Header.js
+++ Header.js
@@ -1,11 +1,11 @@
-// Header.js
-import React from 'react';
+// client/src/Header.js
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { useCart } from './context/CartContext';
 import './Header.css';
 
-const Header = () => {
+const Header = ({ isLoggedIn, setIsLoggedIn }) => { // Add setIsLoggedIn as a prop
   const { getCartCount } = useCart();
   const navigate = useNavigate();
 
@@ -19,6 +19,11 @@ const Header = () => {
         footer.scrollIntoView({ behavior: 'smooth' });
       }
     }, 100); // Allow navigation to home page before scrolling
+  };
+
+  const handleLogout = () => {
+    // Update the login state when user clicks Logout
+    setIsLoggedIn(false);
   };
 
   return (
@@ -39,21 +44,24 @@ const Header = () => {
         </a>
       </nav>
       <div className="header-right">
-        <div className="login-dropdown">
-          <span className="login-signup">Login/Sign Up</span>
-          <div className="dropdown-content">
-            <NavLink to="/login-customer" className="dropdown-item">Customer</NavLink>
-            <NavLink to="/vendorregistrationform" className="dropdown-item">Seller</NavLink>
+        {!isLoggedIn ? ( // Conditional rendering based on isLoggedIn
+          <div className="login-dropdown">
+            <span className="login-signup">Login/Sign Up</span>
+            <div className="dropdown-content">
+              <NavLink to="/login-customer" className="dropdown-item">Customer</NavLink>
+              <NavLink to="/vendorregistrationform" className="dropdown-item">Seller</NavLink>
+            </div>
           </div>
-        </div>
-        <div className="user-dropdown">
-          <FaUser />
-          <div className="dropdown-content">
-            <NavLink to="/profile" className="dropdown-item" activeClassName="active">Profile</NavLink>
-            <NavLink to="/orders" className="dropdown-item" activeClassName="active">Orders</NavLink>
-            <NavLink to="/logout" className="dropdown-item" activeClassName="active">Logout</NavLink>
+        ) : (
+          <div className="user-dropdown">
+            <FaUser />
+            <div className="dropdown-content">
+              <NavLink to="/profile" className="dropdown-item" activeClassName="active">Profile</NavLink>
+              <NavLink to="/orders" className="dropdown-item" activeClassName="active">Orders</NavLink>
+              <span className="dropdown-item" onClick={handleLogout}>Logout</span> {/* Use a span for Logout */}
+            </div>
           </div>
-        </div>
+        )}
         <NavLink to="/cart" className="cart">
           <FaShoppingCart />
           <span className="cart-count">{getCartCount()}</span>
